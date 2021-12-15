08b9e3268c7d93a6a2c88e67fecc68656095066a
Kaiwen Mo <carelven@hotmail.com>
2021-12-15 16:41:54 +0800

    Add files via upload

---
+++ ProxyPowerShell/StopProxy.ps1
@@ -0,0 +1,3 @@
+﻿Get-Process -Name "CCProxy" | Stop-Process
+Get-Process -Name "lantern" | Stop-Process
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name AutoConfigURL -Value ""
